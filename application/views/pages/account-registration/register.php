8c6dec72842d6b7273fab6af7ea2df5012bfc517
Al Mixev Victoro <al.mixev@gmail.com>
2019-10-15 00:45:52 +0700

    Move validation function
    Move all validation function to separate JS

--- application/views/pages/account-registration/register.php
+++ application/views/pages/account-registration/register.php
@@ -123,59 +123,6 @@
 		uiLibrary: 'bootstrap4'
 	});
 
-	function resetValidation() {
-		$(".needs-validation").removeClass("was-validated");
-		console.log('validation reset');
-	}
-
-	function checkPassword() {
-
-		var upperCase= new RegExp('[A-Z]');
-		var lowerCase= new RegExp('[a-z]');
-		var numbers = new RegExp('[0-9]');
-
-		var input = $('#uPass').val();
-
-		if(input.match(upperCase) && input.match(lowerCase) && input.match(numbers)) {
-			$("#uPass").removeClass("is-invalid").addClass("is-valid");
-		} else {
-			$('#uPass').addClass("is-invalid");
-		}
-
-	}
-
-	function matchPassword() {
-
-		var password = $('#uPass').val();
-		var confirmPassword = $('#uPass2').val();
-
-		if(password.match(confirmPassword)) {
-			$("#uPass2").removeClass("is-invalid").addClass("is-valid");
-		} else {
-			$('#uPass2').addClass("is-invalid");
-		}
-
-	}
-
-	function checkExistingEmail() {
-
-		var email = $('#uEmail').val();
-
-		$.ajax({
-			url: "<?php echo base_url('Register/checkExistingEmail'); ?>",
-			type: "GET",
-			data: { email:email }, 
-			success: function(result){
-		    	if(result === 'existing') {
-		    		$('#uEmail').addClass("is-invalid");
-		    	} else {
-		    		$("#uEmail").removeClass("is-invalid").addClass("is-valid");
-		    	}
-		  	}
-		 });
-
-	}
-
 	// Example starter JavaScript for disabling form submissions if there are invalid fields
 	(function() {
 	  'use strict';
--- assets/incube-assets/function.js
+++ assets/incube-assets/function.js
@@ -2,22 +2,64 @@
   INCUBE JAVASCRIPT
 
 
-  1. Login Sliding Page
+  1. Registration Validation
 
 */
 
 
-/*
-  1. Login Sliding Page
+/* 
+	1. Registration Validation Section
 */
 
-function openLogin() {
-  console.log('open the window now');
-  $("#mobile-login-page").animate({right: '0px'}, 'slow');
-}
+	function resetValidation() {
+		$(".needs-validation").removeClass("was-validated");
+		console.log('validation reset');
+	}
 
-function closeLogin() {
-  console.log('close the window');
-  $("#mobile-login-page").animate({left: '-430px'}, 'slow');
+	function checkPassword() {
 
-}
+		var upperCase= new RegExp('[A-Z]');
+		var lowerCase= new RegExp('[a-z]');
+		var numbers = new RegExp('[0-9]');
+
+		var input = $('#uPass').val();
+
+		if(input.match(upperCase) && input.match(lowerCase) && input.match(numbers)) {
+			$("#uPass").removeClass("is-invalid").addClass("is-valid");
+		} else {
+			$('#uPass').addClass("is-invalid");
+		}
+
+	}
+
+	function matchPassword() {
+
+		var password = $('#uPass').val();
+		var confirmPassword = $('#uPass2').val();
+
+		if(password.match(confirmPassword)) {
+			$("#uPass2").removeClass("is-invalid").addClass("is-valid");
+		} else {
+			$('#uPass2').addClass("is-invalid");
+		}
+
+	}
+
+	function checkExistingEmail() {
+
+		var email = $('#uEmail').val();
+
+		$.ajax({
+			url: "<?php echo base_url('Register/checkExistingEmail'); ?>",
+			type: "GET",
+			data: { email:email }, 
+			success: function(result){
+		    	if(result === 'existing') {
+		    		$('#uEmail').addClass("is-invalid");
+		    	} else {
+		    		$("#uEmail").removeClass("is-invalid").addClass("is-valid");
+		    	}
+		  	}
+		 });
+
+	}
